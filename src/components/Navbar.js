6c6b4634c17ea8969b44133f15f5236490f69dbd
Agostina Pielvitori <agos.m.g@gmail.com>
2022-01-15 17:29:32 -0300

    segunda entrega

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar() {
+    return <nav> LOGO 
+        <a> Home</a>
+        <a> Nosotros</a>
+        <a> Galeria</a>
+        <a> Horarios </a>
+    </nav>;
+}
+
+export default Navbar;
